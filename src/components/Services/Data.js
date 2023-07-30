2be5d8bb55057faec35f2cab9b1fb52d8737c8e9
Josh-Hawley <74909481+Josh-Hawley@users.noreply.github.com>
2023-07-30 23:34:57 +0100

    Update Data.js

--- src/components/Services/Data.js
+++ src/components/Services/Data.js
@@ -7,7 +7,7 @@ export const servicesData = {
     lightTextDesc: true,
     topLine: 'Graphic design for science',
     headline: 'Our services',
-    description: 'Effective graphic design can simplify the complexity of scientific data in an efficient and intuitive way that engages the reader. This in turn encourages collaboration and wider scientific understanding and can be utilised across many different areas of scientific communication.',
+    description: 'Specialising in biology, we provide illustration services to visually represent, summarise, and communicate your scientific research. Our aim is to promote scientific understanding and foster collaboration by transforming complex data into intuitive visual elements, providing accurate and insightful summaries.',
     buttonLabel: 'Request a quote',
     imgStart: false,
     img: require('../../images/Gallery/Neurulation.jpg'), // .default was important here, doesn't load image without 
