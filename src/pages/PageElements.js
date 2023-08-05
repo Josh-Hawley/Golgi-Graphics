f7b5ff3e9cba26be93169df8cab5c26422ac3725
Josh H <joshhawley369@gmail.com>
2023-08-05 19:01:37 +0100

    404 error

--- src/pages/PageElements.js
+++ src/pages/PageElements.js
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
-import { theme } from '../Theme';
+
 export const PageContainer = styled.div`
-position: absolute;
-height: 100vh;
-width: 100%;
-background-color: ${theme.colors.lighterBg};
-transition: 0.3s cubic-bezier(.62,.13,.54,.85);
-left: ${({ isOpen }) => (isOpen ? '-50%' : '0%')};
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    transition: 0.3s cubic-bezier(.62,.13,.54,.85);
+    left: ${({ isOpen }) => (isOpen ? '-50%' : '0%')};
 `
