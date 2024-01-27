2359d4572113936a24fc4103b31045c433c59df7
Josh H <joshhawley369@gmail.com>
2024-01-27 16:23:41 +0000

    updated gallery

--- src/components/PortfolioPg/data.js
+++ src/components/PortfolioPg/data.js
@@ -3,10 +3,11 @@ import img2 from '../../images/Gallery/img3.png';
 import img1 from '../../images/Gallery/Neurulation.jpg';
 import img8 from '../../images/Gallery/HES5 pattern neural tube.jpg';
 import img9 from '../../images/Gallery/Memrbane fluorescence art.jpg';
-import img10 from '../../images/Gallery/HES neuroepithelia art.jpg';
+// import img10 from '../../images/Gallery/HES neuroepithelia art.jpg';
 import img11 from '../../images/Gallery/IMG_2482.jpeg';
 import img13 from '../../images/Gallery/grant figure.jpg';
 import img14 from '../../images/Gallery/AnzyGraphicalAbstract.jpg';
+import img15 from '../../images/Gallery/PosterTheNotchMeeting.jpg';
 
 import vid1 from '../../videos/vesicle rotating.mp4';
 import vid2 from '../../videos/potassium channel.mp4';
@@ -14,6 +15,16 @@ import vid2 from '../../videos/potassium channel.mp4';
 
 
 export const images = [
+  
+  {
+    id: 14,
+    type: 'image',
+    src: img14,
+    title: 'Graphical abstract',
+    tags: ['Graphical abstract'],
+    description: 'Graphical abstract for a paper soon to be published.'
+  },
+  
   {
     id: 12,
     type: 'video',
@@ -31,6 +42,23 @@ export const images = [
     description: 'The process of neurulation. The ectoderm forms the spinal cord by first folding in on itself to form the neural tube, which is made up of neural progenitors that go on to form the neurons and glia of the spinal cord, brain, and retina.'
   },
   {
+    id: 15,
+    type: 'image',
+    src: img15,
+    title: 'Conference Poster',
+    tags: ['Poster'],
+    description: 'Poster summarising my own research, presented at The Notch Meeting in Athens, 2023.'
+  },
+
+  {
+    id: 13,
+    type: 'image',
+    src: img13,
+    title: 'Grant figure',
+    tags: ['Grant figure'],
+    description: 'Work done for the graphical abstract of a grant application.'
+  },
+  {
     id: 2,
     src: img2,
     type: 'image',
@@ -39,6 +67,9 @@ export const images = [
     description: 'Notch signalling is a contact dependent signalling network utilised across many developmental processes as well as in adult tissue. Here, it is shown in the context of its regulation on Hes and proneural genes.'
   },
   
+
+  
+
   {
     id: 3,
     src: img3,
@@ -63,19 +94,19 @@ export const images = [
     type: 'image',
     title: 'Neuroepithelia',
     tags: ['Grant figure', 'Paper'],
-    description: 'The structure of the neuroepithelia that make up the neural tube, and the patterned gene expression within it.'
+    description: 'Figure made for a grant application. The structure of the neuroepithelia that make up the neural tube, and the patterned gene expression within it.'
   },
 
 
 
-  {
-    id: 10,
-    type: 'image',
-    src: img10,
-    title: 'Lightbulb cell art',
-    tags: ['Art', '3D', 'Cells'],
-    description: 'Artistic 3D render of endogenous HES5::Venus reporter in neuroepithelia.'
-  },
+  // {
+  //   id: 10,
+  //   type: 'image',
+  //   src: img10,
+  //   title: 'Lightbulb cell art',
+  //   tags: ['Art', '3D', 'Cells'],
+  //   description: 'Artistic 3D render of endogenous HES5::Venus reporter in neuroepithelia.'
+  // },
 
   {
     id: 9,
@@ -86,23 +117,9 @@ export const images = [
     description: 'Artistic 3D render of mosaically labelled neuroepithlial cells where bright cells represent a membrane-bound fluorescent protein.'
   },
 
-  {
-    id: 13,
-    type: 'image',
-    src: img13,
-    title: 'Grant figure',
-    tags: ['Grant figure'],
-    description: 'Work done for the graphical abstract of a grant application.'
-  },
+  
 
-  {
-    id: 14,
-    type: 'image',
-    src: img14,
-    title: 'Graphical abstract',
-    tags: ['Graphical abstract'],
-    description: 'Graphical abstract for a paper soon to be published.'
-  },
+  
 
   {
     id: 11,
@@ -113,6 +130,7 @@ export const images = [
     description: 'V-ATPase - a proton pump - on the surface of a lysosome, roughly to scale for a small vesicle.'
   },
 
+ 
   
 
   
