0c00676a824f88ca24cb808e65792cc9c460fd18
Josh H <joshhawley369@gmail.com>
2025-07-24 10:25:57 +0100

    update to gallery

--- src/components/PortfolioPg/data.js
+++ src/components/PortfolioPg/data.js
@@ -7,10 +7,10 @@ import img1 from '../../images/Gallery/Neurulation.jpg';
 import img16 from '../../images/Gallery/synaptic vesicle.jpg';
 // import img17 from '../../images/Gallery/The tumour microenvironment.jpg';
 import img18 from '../../images/Gallery/PhD process.jpg';
-import img19 from '../../images/Gallery/Glass bottles rough.jpg';
+// import img19 from '../../images/Gallery/Glass bottles rough.jpg';
 import img20 from '../../images/Gallery/Cytonemes.jpg';
-import img21 from '../../images/Gallery/SunTag system.jpg';
-import img22 from '../../images/Gallery/BoneFinder.jpg';
+// import img21 from '../../images/Gallery/SunTag system.jpg';
+// import img22 from '../../images/Gallery/BoneFinder.jpg';
 // import img23 from '../../images/Gallery/Spatial translatomics.jpg';
 // import img24 from '../../images/Gallery/Translational toolkit flyer.jpg';
 // import img25 from '../../images/Gallery/Translation toolkit logo.jpg';
@@ -19,15 +19,16 @@ import img26 from '../../images/Gallery/Biocentric webpage.jpg';
 import img28 from '../../images/Gallery/Banner.jpg';
 import img29 from '../../images/Gallery/BMP signalling.jpg';
 import img30 from '../../images/Gallery/ISRU-logo.jpg';
-import img31 from '../../images/Gallery/FBMH Technology Platforms Logo.jpg';
+// import img31 from '../../images/Gallery/FBMH Technology Platforms Logo.jpg';
 // import img32 from '../../images/Gallery/The Basement Membrane at a Glance.jpg';
-import img33 from '../../images/Gallery/SunTag system.jpg';
+// import img33 from '../../images/Gallery/SunTag system.jpg';
 // import img34 from '../../images/Gallery/Spatial translatomics.jpg';
-import img35 from '../../images/Gallery/Helices-all-the-way-down.jpg';
+// import img35 from '../../images/Gallery/Helices-all-the-way-down.jpg';
 import img36 from '../../images/Gallery/Matrix-centre-posters.jpg';
 
 
 // import vid1 from '../../videos/vesicle rotating.mp4';
+import vid1 from '../../videos/Vesicle_fusion.mp4';
 import vid2 from '../../videos/IKNM.mp4';
 import vid3 from '../../videos/K-channel compressed.mp4';
 import vid4 from '../../videos/Ligand receptor.mp4';
@@ -35,6 +36,16 @@ import vid4 from '../../videos/Ligand receptor.mp4';
 
 
 export const images = [
+
+
+  {
+    id: 50,
+    src: vid1,
+    type: 'video',
+    title: 'Exocytosis & endocytosis',
+    tags: ['3D', 'Animation'],
+    description: 'An animation showing vesicles fusing to a cellular membrane. This was a personal project with the aim of highlighting the merging and displacement of lipids when fusion occurs (highlighted with the various colours of lipids).'
+  },
 
   {
   id: 1,
@@ -45,14 +56,17 @@ export const images = [
   description: 'Synaptic vesicles are absolutely packed with glutamate - around 8000 molecules on average - this concentration is depicted here in a 40nm diameter vesicle. Also shown are some of the main vesicle proteins present in their reported average abundance, such as V-ATPase, glutamate transporters and SNARE complexes.'
 },
 
+
   {
-    id: 14,
-    src: vid3,
-    type: 'video',
-    title: 'Potassium ion channel',
-    tags: ['3D', 'Animation'],
-    description: 'A procedural animation of a phospholipid bilayer, potassium ion channels and potassium ions passing through them.'
+    id: 39,
+    src: img20,
+    type: 'image',
+    title: 'Cytonemes in the germarium niche',
+    tags: ['Figure'],
+    description: 'How cytonemes influence signalling in the germarium niche.'
   },
+  
+
 
 
 
@@ -65,6 +79,14 @@ export const images = [
     description: 'Depicted are two cells, one with Notch receptors (red), and the other with Delta ligands (blue). This short animation uses data from the Protein Data Bank to show an accurate final binding position of the receptor and ligand. The membrane is procedurally animated.'
   },
 
+      {
+  id: 44,
+  src: img36,
+  type: 'image',
+  title: 'Wellcome Trust Cell-Matrix Centre 30th anniversary posters',
+  tags: ['Poster'],
+  description: 'This set of posters was designed for the Wellcome Trust Cell-Matrix Centre\'s 30th anniversary event that took place at The University of Manchester on the 25th June 2025. The Cell-Matrix centre is comprised of 30 research groups that broadly work on three distinct research areas: Mechano-matrix, Immuno-matrix, and Chrono-matrix. Therefore the idea behind the posters was to highlight these three areas in a visually compelling way while also providing more technical depth by the inclusion of graphical abstracts describing a selection of the research that has been carried out by the groups at the centre.'
+},
   
   {
     id: 4,
@@ -78,44 +100,30 @@ export const images = [
   
 
 
-  {
-    id: 102,
-    src: img21,
-    type: 'image',
-    title: 'SunTag system',
-    tags: ['Figure'],
-    description: 'Figure for a client which outlines ongoing research soon to be published. Please do not re-use or repost this image.'
-  },
+  // {
+  //   id: 102,
+  //   src: img21,
+  //   type: 'image',
+  //   title: 'SunTag system',
+  //   tags: ['Figure'],
+  //   description: 'Figure for a client which outlines ongoing research soon to be published. Please do not re-use or repost this image.'
+  // },
 
 
    
   
-{
-    id: 16,
-    type: 'image',
-    src: img19,
-    title: 'Spinal cord stem cells',
-    tags: ['Art', '3D'],
-    description: 'An abstract still of the 3D file used to make the neuroepithelia animation featured on this page. The desired effect was backlit frosted glass.'
-  },
+// {
+//     id: 16,
+//     type: 'image',
+//     src: img19,
+//     title: 'Spinal cord stem cells',
+//     tags: ['Art', '3D'],
+//     description: 'An abstract still of the 3D file used to make the neuroepithelia animation featured on this page. The desired effect was backlit frosted glass.'
+//   },
 
-  {
-  id: 34,
-  src: img30,
-  type: 'image',
-  title: 'UK In-Situ Resource Utilisation logo',
-  tags: ['Logo'],
-  description: 'A logo developed for the UK In-Situ Resource Utilisation (ISRU UK) group who are funded by the UK Space Agency. They represent research in the UK that concerns space exploration resource utilisation.'
-},
 
-    {
-  id: 44,
-  src: img36,
-  type: 'image',
-  title: 'Wellcome Trust Cell-Matrix Centre 30th anniversary posters',
-  tags: ['Poster'],
-  description: 'This set of posters was designed for the Wellcome Trust Cell-Matrix Centre\'s 30th anniversary event that took place at The University of Manchester on the 25th June 2025. The Cell-Matrix centre is comprised of 30 research groups that broadly work on three distinct research areas: Mechano-matrix, Immuno-matrix, and Chrono-matrix. Therefore the idea behind the posters was to highlight these three areas in a visually compelling way while also providing more technical depth by the inclusion of graphical abstracts describing a selection of the research that has been carried out by the groups at the centre.'
-},
+
+
   
    {
     id: 10,
@@ -128,6 +136,24 @@ export const images = [
 
  
  
+    {
+    id: 14,
+    src: vid3,
+    type: 'video',
+    title: 'Potassium ion channel',
+    tags: ['3D', 'Animation'],
+    description: 'A procedural animation of a phospholipid bilayer, potassium ion channels and potassium ions passing through them.'
+  },
+
+  {
+    id: 13,
+    type: 'image',
+    src: img18,
+    title: 'The PhD process',
+    tags: ['Art'],
+    description: 'Concept art for a PhD recruitment advert. This image depicts the journey of taking on a scientific research area during a PhD.'
+  },
+
 {
   id: 36,
   src: img29,
@@ -154,26 +180,26 @@ export const images = [
 //   description: 'A poster designed for an article submitted to '
 // },
 
-{
-  id: 35,
-  src: img31,
-  type: 'image',
-  title: 'FBMH Technology Platforms logo',
-  tags: ['Logo'],
-  description: 'A logo designed for the Faculty of Biology, Medicine and Health (FBMH) Technology Platforms at the University of Manchester. <a href="https://www.bmh.manchester.ac.uk/research/platforms/" style="font-size: 0.9rem;">Link to FBMH Technology Platforms.</a>'
-},
+// {
+//   id: 35,
+//   src: img31,
+//   type: 'image',
+//   title: 'FBMH Technology Platforms logo',
+//   tags: ['Logo'],
+//   description: 'A logo designed for the Faculty of Biology, Medicine and Health (FBMH) Technology Platforms at the University of Manchester. <a href="https://www.bmh.manchester.ac.uk/research/platforms/" style="font-size: 0.9rem;">Link to FBMH Technology Platforms.</a>'
+// },
 
 
 
 
-{
-  id: 41,
-  src: img33,
-  type: 'image',
-  title: 'SunTag system',
-  tags: ['Figure'],
-  description: 'A figure describing the SunTag System.'
-},
+// {
+//   id: 41,
+//   src: img33,
+//   type: 'image',
+//   title: 'SunTag system',
+//   tags: ['Figure'],
+//   description: 'A figure describing the SunTag System.'
+// },
 
 
 
@@ -196,46 +222,42 @@ export const images = [
   description: 'Webpage banner designed for the Translation Toolkit at The University of Manchester. <a href="https://translation-toolkit.manchester.ac.uk/" style="font-size: 0.9rem;">Link to Translation Toolkit.</a>'
 },
   
+  
+
+ 
+
+
+
+
+
+// {
+//   id: 43,
+//   src: img35,
+//   type: 'image',
+//   title: 'Helices all the way down',
+//   tags: ['Art'],
+//   description: 'Stylised helical structures found in cells: microtubules, DNA, actomyosin, collagen, and alpha helices.'
+// },
+
+
+// {
+//   id: 30,
+//   src: img22,
+//   type: 'image',
+//   title: 'Rollout banner',
+//   tags: ['Promotional'],
+//   description: 'Rollout banner designed for a conference.'
+// },
+
   {
-    id: 13,
-    type: 'image',
-    src: img18,
-    title: 'The PhD process',
-    tags: ['Art'],
-    description: 'Concept art for a PhD recruitment advert. This image depicts the journey of taking on a scientific research area during a PhD.'
-  },
-
-   {
-    id: 39,
-    src: img20,
-    type: 'image',
-    title: 'Cytonemes in the germarium niche',
-    tags: ['Figure'],
-    description: 'How cytonemes influence signalling in the germarium niche.'
-  },
-
-
-
-
-
-{
-  id: 43,
-  src: img35,
+  id: 34,
+  src: img30,
   type: 'image',
-  title: 'Helices all the way down',
-  tags: ['Art'],
-  description: 'Stylised helical structures found in cells: microtubules, DNA, actomyosin, collagen, and alpha helices.'
+  title: 'UK In-Situ Resource Utilisation logo',
+  tags: ['Logo'],
+  description: 'A logo developed for the UK In-Situ Resource Utilisation (ISRU UK) group who are funded by the UK Space Agency. They represent research in the UK that concerns space exploration resource utilisation.'
 },
 
-
-{
-  id: 30,
-  src: img22,
-  type: 'image',
-  title: 'Rollout banner',
-  tags: ['Promotional'],
-  description: 'Rollout banner designed for a conference.'
-},
 {
   id: 33,
   src: img26,
